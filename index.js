986700d8a19e5b78c4c2a035fa6f845dc01a40f5
Quinn Sissler <QSissler@gmail.com>
2023-06-23 15:15:26 -0400

    commit tree d1c7cebcc5c59df53acc54db687a03c0cd051b1d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+
+ cats.push(name);
+}
+function destructivelyPrependCat(name) {
+
+    cats.unshift(name);
+   }
+function destructivelyRemoveLastCat(name) {
+
+    cats.pop(name);
+   }
+function destructivelyRemoveFirstCat(name) {
+
+    cats.shift(name);
+   }
+function appendCat(name) {
+    const allCats = [...cats];
+    allCats.push(name);
+    return allCats;
+   }
+function prependCat(name) {
+    const allCats = [...cats];
+    allCats.unshift(name);
+    return allCats;
+   } 
+ function removeLastCat() {
+   return cats.slice(0, cats.length - 1);
+   } 
+ function removeFirstCat() {
+    return cats.slice(1);
+   }
